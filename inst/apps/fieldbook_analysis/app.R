d4d2d25275fd3deedee93a6c0e1f28b077e2113f
Frojast <rsimon64@gmail.com>
2016-08-31 14:31:20 -0500

    updated setup for brapi

--- inst/apps/fieldbook_analysis/app.R
+++ inst/apps/fieldbook_analysis/app.R
@@ -30,62 +30,7 @@ ui <- dashboardPage(skin = "yellow",
                     dashboardBody(
                       tabItems(
                         tabItem(tabName = "tab_analysis",
-                                fluidRow(
-                                  column(width = 12,
-                                         box(width = NULL,
-                                             title = "Fieldbook",
-                                             shinyFiles::shinyFilesButton('fbaInput', 'File select',
-                                                                          'Please select a fieldbook file', FALSE
-                                             ),
-                                             #p(class = 'text-center', downloadButton('locsDL', 'Download Filtered Data')),
-                                             #uiOutput("fbList"),
-                                             uiOutput("ui_set_plt"),
-                                             uiOutput("ui_set_rep"),
-                                             uiOutput("ui_set_gen"),
-                                             DT::dataTableOutput("hotFieldbook")
-                                             #locationsUI("location")
-                                         )
-                                  )
-                                  # ,
-                                  # column(width = 4,
-                                  #        tabBox(width = NULL, title = "Site",
-                                  #               tabPanel("Histogram",
-                                  #                        plotOutput("histogram")
-                                  #               ),
-                                  #               tabPanel("Info",
-                                  #                        htmlOutput("siteInfo")
-                                  #               ), tabPanel("Fieldtrials",
-                                  #                           htmlOutput("site_fieldtrials")
-                                  #               ),
-                                  #               tabPanel("Genotypes",
-                                  #                        htmlOutput("site_genotypes")
-                                  #               )
-                                  #
-                                  #        )
-                                  # )
-                                )
-                                ,
-                                fluidRow(
-                                  column(width = 12,
-                                         tabBox(width = NULL, selected = "Map",# id = "tabAnalysis",
-                                                tabPanel("Correlation",
-                                                         qtlcharts::iplotCorr_output('vcor_output', height = 400)
-                                                ),
-                                                tabPanel("Map",
-                                                         d3heatmap::d3heatmapOutput("fieldbook_heatmap")
-                                                ),
-                                                tabPanel(title = "Report",
-                                                         #htmlOutput("fb_report")
-                                                         htmlOutput("fbRep")
-
-                                                )
-
-
-
-                                         )
-                                  )
-
-                                )
+                            brapps::fbasingle_ui("Single")
                         )
 
                       )
@@ -96,10 +41,15 @@ ui <- dashboardPage(skin = "yellow",
 
 sv <- function(input, output, session) ({
 
-  values <- reactiveValues()
+  values <- shiny::reactiveValues(crop = "sweetpotato", amode = "brapi")
+
+  brapi_con("sweetpotato", "sgn:eggplant@sweetpotatobase-test.sgn.cornell.edu",
+            80, "rsimon16",
+            "sweetpotato")
+
 
   #shinyURL.server()
-  brapps::fieldbook_analysis_file(input, output, session, values)
+  brapps::fieldbook_analysis(input, output, session, values)
 })
 
 shinyApp(ui, sv)
